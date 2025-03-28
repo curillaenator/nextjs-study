e97c02b23447ad66dbf3a5c4259faafb159695ab
KirillArt <curillaenator@gmail.com>
2025-03-28 15:36:20 +0700

    fix: server data

---
+++ app/blog/error.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styles from '../blog.module.scss';
+
+export default function ErrorWrapper({ error }: { error: Error }) {
+  return (
+    <div className={styles.blog}>
+      <span>{`Oops... ${error.message}`}</span>
+    </div>
+  );
+}
---
+++ app/blog/loading.tsx
@@ -0,0 +1,5 @@
+import styles from './blog.module.scss';
+
+export default function LoadingPosts() {
+  return <div className={styles.loader}>Loading...</div>;
+}
