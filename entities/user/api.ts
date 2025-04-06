918d55334be84cf593429835d5be3c9392fa02b3
KirillArt <curillaenator@gmail.com>
2025-04-06 09:25:59 +0700

    feat: lint on commits

---
+++ entities/user/api.ts
@@ -0,0 +1,17 @@
+import type { NextAppUser } from './interfaces';
+
+const asd = null;
+
+const USERS_ENDPOINT = 'http://localhost:4000/users';
+
+const authUser = async (creds: Record<'email' | 'password', string>) => {
+  const data = await fetch(USERS_ENDPOINT, {
+    next: { revalidate: 15 },
+  });
+
+  const userList = (await data.json()) as NextAppUser[];
+
+  return userList.find(({ email, password }) => creds.email === email && creds.password === password) || null;
+};
+
+export { authUser };
